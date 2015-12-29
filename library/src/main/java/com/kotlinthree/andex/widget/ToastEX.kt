04979b930a1500fcef07436740e86ffe87c9b0b4
heliangwei <heliangwei@hujiang.com>
2015-12-29 15:23:40 +0800

    modify: toast && resource extension

--- library/src/main/java/com/kotlinthree/andex/widget/ToastEX.kt
+++ library/src/main/java/com/kotlinthree/andex/widget/ToastEX.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2015  Ohmer.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.kotlinthree.andex.widget
 
 import android.view.Gravity
@@ -6,7 +21,6 @@ import android.widget.Toast
 
 /**
  * Created by ohmer on 12/28/15.
- *
  */
 
 /**
